498a63e905d1b4f40d261609efc792bfb0b5d7f1
Sean Houde <seanhoude@gmail.com>
2018-11-05 14:28:27 -0500

    adding 2nd test

--- currency.py
+++ currency.py
@@ -1,3 +1,14 @@
+rates = [('EUR', 'USD', 1.13966), ('USD', 'JPY', 113.215), ('USD', 'CAD', 1.31003), ('EUR', 'JPY', 129.027), 
+('USD', 'EUR', 0.877), ('JPY', 'USD', 0.00883), ('CAD', 'USD', 0.763), ('EUR', 'CAD', 1.492), ('JPY', 'CAD', 0.0115),
+('JPY', 'EUR', 0.00775), ('CAD', 'EUR', 0.66987), ('CAD', 'JPY', 86.404)]
+
+
 def convert(_value, _from, _to):
     if _from == _to:
         return _value
+    else:
+        for tup in rates:
+            if tup[0] == _from:
+                if tup[1] == _to:
+                    rate = tup[2]
+        return _value * rate
--- test_currency.py
+++ test_currency.py
@@ -1,14 +1,25 @@
 from currency import convert
 
 
-rates = [('EUR', 'USD', 1.13966), ('USD', 'JPY', 113.215), ('USD', 'CAD', 1.31003), ('EUR', 'JPY', 129.027), 
-('USD', 'EUR', 0.877), ('JPY', 'USD', 0.00883), ('CAD', 'USD', 0.763), ('EUR', 'CAD', 1.492), ('JPY', 'CAD', 0.0115),
-('JPY', 'EUR', 0.00775), ('CAD', 'EUR', 0.66987), ('CAD', 'JPY', 86.404)]
+rates = [
+    ('EUR', 'USD', 1.13966),
+    ('USD', 'JPY', 113.215),
+    ('USD', 'CAD', 1.31003),
+    ('EUR', 'JPY', 129.027), 
+    ('USD', 'EUR', 0.877),
+    ('JPY', 'USD', 0.00883),
+    ('CAD', 'USD', 0.763),
+    ('EUR', 'CAD', 1.492),
+    ('JPY', 'CAD', 0.0115),
+    ('JPY', 'EUR', 0.00775),
+    ('CAD', 'EUR', 0.66987),
+    ('CAD', 'JPY', 86.404)
+]
 
 
 def test_convert():
     assert convert(1, "USD", "USD") == 1
-    assert convert(7, "CAD", "CAD") == 7
+    assert convert(1, "USD", "EUR") == 0.877
 
 
 # (_from, to, value) = rates[0]
