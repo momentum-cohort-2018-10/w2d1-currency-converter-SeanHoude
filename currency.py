9aef792986872c2e6a895a8dd6984e53f212c3e7
Sean Houde <seanhoude@gmail.com>
2018-11-05 14:07:56 -0500

    Adding first test

---
+++ currency.py
@@ -0,0 +1,3 @@
+def convert(_value, _from, _to):
+    if _from == _to:
+        return _value
---
+++ test_currency.py
@@ -0,0 +1,17 @@
+from currency import convert
+
+
+rates = [('EUR', 'USD', 1.13966), ('USD', 'JPY', 113.215), ('USD', 'CAD', 1.31003), ('EUR', 'JPY', 129.027), 
+('USD', 'EUR', 0.877), ('JPY', 'USD', 0.00883), ('CAD', 'USD', 0.763), ('EUR', 'CAD', 1.492), ('JPY', 'CAD', 0.0115),
+('JPY', 'EUR', 0.00775), ('CAD', 'EUR', 0.66987), ('CAD', 'JPY', 86.404)]
+
+
+def test_convert():
+    assert convert(1, "USD", "USD") == 1
+    assert convert(7, "CAD", "CAD") == 7
+
+
+# (_from, to, value) = rates[0]
+
+# print(rates[0])
+# print(_from)
